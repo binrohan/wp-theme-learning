f22b0baf74f40227ccf9d963eddfc5cf3f088ae5
binrohan <binrohan.cs@gmail.com>
2021-12-20 00:28:41 +0600

    7. Workflow and Automation

--- functions.php
+++ functions.php
@@ -1,18 +1,23 @@
 <?php
 
 function university_files() {
-  wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true);
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-  wp_enqueue_style('university_main_styles', get_stylesheet_uri(), NULL, microtime());
+  
+  if (strstr($_SERVER['SERVER_NAME'], 'http://amazingcollege.local')) {
+    wp_enqueue_script('main-university-js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
+  } else {
+    wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendors~scripts.4c6b6356b733c0fdb361.js'), NULL, '1.0', true);
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.641d80401e1738eb718c.js'), NULL, '1.0', true);
+    wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.641d80401e1738eb718c.css'));
+  }
+
+  
 }
 
 add_action('wp_enqueue_scripts', 'university_files');
 
 function university_features() {
-  register_nav_menu('headerMenuLocation', 'Header Menu Location');
-  // register_nav_menu('footerMenuLocationOne', 'Footer Location One');
-  // register_nav_menu('footerMenuLocationTwo', 'Footer Location Two');
   add_theme_support('title-tag');
 }
 
