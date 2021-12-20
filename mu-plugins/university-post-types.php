5aa6db42cb203ccdd006d6194f0bf55b1d474803
binrohan <binrohan.cs@gmail.com>
2021-12-21 02:51:22 +0600

    9. Programs Post Type

--- mu-plugins/university-post-types.php
+++ mu-plugins/university-post-types.php
@@ -1,5 +1,6 @@
 <?php
 function university_post_types() {
+  // Event Post Type
   register_post_type('event', array(
     'rewrite' => array('slug' => 'events'),
     'show_in_rest' => true,
@@ -15,6 +16,57 @@ function university_post_types() {
     ),
     'menu_icon' => 'dashicons-calendar'
     
+  ));
+
+  // Program Post Type
+  register_post_type('program', array(
+    'rewrite' => array('slug' => 'programs'),
+    'show_in_rest' => true,
+    'supports' => array('title', 'editor'),
+    'has_archive' => true,
+    'public' => true,
+    'labels' => array(
+      'name' => 'Programs',
+      'add_new_item' => 'Add New Program',
+      'edit_item' => 'Edit Program',
+      'all_items' => 'All Programs',
+      'singular_name' => 'Program'
+    ),
+    'menu_icon' => 'dashicons-awards'
+  ));
+
+  // Professor Post Type
+  register_post_type('professor', array(
+    'rewrite' => array('slug' => 'professors'),
+    'show_in_rest' => true,
+    'supports' => array('title', 'editor'),
+    'has_archive' => true,
+    'public' => true,
+    'labels' => array(
+      'name' => 'Professors',
+      'add_new_item' => 'Add New Professor',
+      'edit_item' => 'Edit Professor',
+      'all_items' => 'All Professors',
+      'singular_name' => 'Professor'
+    ),
+    'menu_icon' => 'dashicons-user'
+  ));
+
+  // Campus Post Type
+  register_post_type('campus', array(
+    'rewrite' => array('slug' => 'Campuses'),
+    'show_in_rest' => true,
+    'supports' => array('title', 'editor'),
+    'has_archive' => true,
+    'public' => true,
+    'labels' => array(
+      'name' => 'Campuses',
+      'add_new_item' => 'Add New Campus',
+      'edit_item' => 'Edit Campus',
+      'all_items' => 'All Campuses',
+      'singular_name' => 'Campus'
+    ),
+    'menu_icon' => 'dashicons-user'
   ));
 }
 
