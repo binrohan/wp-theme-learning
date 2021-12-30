fad9684388871e03fc3a70b2b9c6f87a6f8bbf8c
binrohan <binrohan.cs@gmail.com>
2021-12-31 01:12:02 +0600

    20. Like or Heart Count for Professors

--- themes/fictional-university-theme/functions.php
+++ themes/fictional-university-theme/functions.php
@@ -1,5 +1,6 @@
 <?php
 
+require get_theme_file_path('/inc/like-route.php');
 require get_theme_file_path('/inc/search-route.php');
 
 function university_custom_rest()
@@ -61,8 +62,8 @@ function university_files()
     wp_enqueue_script('main-university-js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
   } else {
     wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendors~scripts.c3ece7e966dcb0202176.js'), NULL, '1.0', true);
-    wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.dca6ba898612d67c8569.js'), NULL, '1.0', true);
-    wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.dca6ba898612d67c8569.css'));
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.f79bddcf2e61e30dd716.js'), NULL, '1.0', true);
+    wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.f79bddcf2e61e30dd716.css'));
   }
 
   wp_localize_script('main-university-js', 'universityData', array(
@@ -159,7 +160,7 @@ function our_login_css()
 
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
 
-  wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.dca6ba898612d67c8569.css'));
+  wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.f79bddcf2e61e30dd716.css'));
 }
 
 add_action('login_enqueue_scripts', 'our_login_css');
--- themes/fictional-university-theme/js/modules/Like.js
+++ themes/fictional-university-theme/js/modules/Like.js
@@ -1,11 +1,63 @@
-import $ from 'jquery';
+import $, { type } from 'jquery';
 
 class Like {
-    constructor(){
-        this.events();
-    }
+  constructor() {
+    this.events();
+  }
 
-    events() {
+  events() {
+    $('.like-box').on('click', this.clickDispatcher.bind(this));
+  }
 
+  clickDispatcher(e) {
+    const likeBox = $(e.target).closest('.like-box');
+
+    if (likeBox.attr('data-exists') == 'yes') {
+      this.deleteLike(likeBox);
+    } else {
+      this.createLike(likeBox);
     }
+  }
+
+  createLike(likeBox) {
+    $.ajax({
+      beforeSend: (xhr) => {
+        xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
+      },
+      url: universityData.root_url + '/wp-json/university/v1/manageLike',
+      type: 'POST',
+      data: { professorId: likeBox.attr('data-id') },
+      success: (response) => {
+        likeBox.attr('data-exists', 'yes');
+        let likeCount = +likeBox.find('.like-count').html();
+        likeBox.find('.like-count').html(++likeCount);
+        likeBox.attr('data-like', response);
+      },
+      error: (response) => {
+        console.log(response);
+      },
+    });
+  }
+
+  deleteLike(likeBox) {
+    $.ajax({
+      beforeSend: (xhr) => {
+        xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
+      },
+      url: universityData.root_url + '/wp-json/university/v1/manageLike',
+      type: 'DELETE',
+      data: { like: likeBox.attr('data-like') },
+      success: (response) => {
+        likeBox.attr('data-exists', 'no');
+        let likeCount = +likeBox.find('.like-count').html();
+        likeBox.find('.like-count').html(--likeCount);
+        likeBox.attr('data-like', '');
+      },
+      error: (response) => {
+        console.log(response);
+      },
+    });
+  }
 }
+
+export default Like;
